0ba828e79958acf2fbce036347ec655a48e5543e
Veit Schwämmle <veitveit@gmail.com>
2018-04-13 10:00:28 +0200

    Update install_packages.R

--- DockerSetup/install_packages.R
+++ DockerSetup/install_packages.R
@@ -2,4 +2,5 @@ options(repos = c(CRAN = "https://cran.rstudio.com"))
 update.packages(ask=F)
 source("https://bioconductor.org/biocLite.R")
 biocLite(ask=F)
+
 biocLite(c("isobar", "limma", "grimbough/Rhdf5lib", "sneumann/mzR","lgatto/MSnbase"))
