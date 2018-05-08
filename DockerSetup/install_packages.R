bc5e9c598d6cddda02003fa56b51ec03ddbec5f7
veitveit <veitveit@gmail.com>
2018-05-08 15:27:12 +0200

    added statistics

--- DockerSetup/install_packages.R
+++ DockerSetup/install_packages.R
@@ -4,7 +4,7 @@ source("https://bioconductor.org/biocLite.R")
 biocLite(ask=F)
 
 packages.to.install <- 
-c("Bioconductor/BiocGenerics","Bioconductor/S4Vectors","Bioconductor/IRanges","grimbough/Rhdf5lib","sneumann/mzR","limma","isobar", "lgatto/MSnbase","plotly","reshape")
+c("Bioconductor/BiocGenerics","Bioconductor/S4Vectors","Bioconductor/IRanges","grimbough/Rhdf5lib","sneumann/mzR","limma","isobar", "lgatto/MSnbase","plotly","reshape","qvalue")
 
 for (package in packages.to.install) {
     biocLite(package)
