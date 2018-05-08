5db02b2d1aa49a7542b772b27cae48511e16f5d0
veitveit <veitveit@gmail.com>
2018-05-08 08:26:08 +0200

    added plotly and reshape libraries

--- DockerSetup/install_packages.R
+++ DockerSetup/install_packages.R
@@ -4,7 +4,7 @@ source("https://bioconductor.org/biocLite.R")
 biocLite(ask=F)
 
 packages.to.install <- 
-c("Bioconductor/BiocGenerics","Bioconductor/S4Vectors","Bioconductor/IRanges","grimbough/Rhdf5lib","sneumann/mzR","limma","isobar", "lgatto/MSnbase","shiny")
+c("Bioconductor/BiocGenerics","Bioconductor/S4Vectors","Bioconductor/IRanges","grimbough/Rhdf5lib","sneumann/mzR","limma","isobar", "lgatto/MSnbase","plotly","reshape")
 
 for (package in packages.to.install) {
     biocLite(package)
